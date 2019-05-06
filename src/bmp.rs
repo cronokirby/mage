5807b2dd3e30f55e58c9da5875260cf6570962c4
Lúcás Meier <cronokirby@gmail.com>
2019-05-06 15:39:35 +0200

    Add function to parse file header

--- src/bmp.rs
+++ src/bmp.rs
@@ -1,6 +1,14 @@
 // The structures and parsing in this module are mainly based off of the
 // following: http://www.dragonwins.com/domains/GetTechEd/bmp/bmpfileformat.htm
 
+/// Parse a little endian integer from a slice of bytes
+///
+/// This function doesn't check size at all, so this should be done
+/// before calling it.
+fn u32_le(data: &[u8]) -> u32 {
+    (data[0] as u32) | ((data[1] as u32) << 8) | ((data[2] as u32) << 16) | ((data[3] as u32) << 24)
+}
+
 /// Represents the errors we can encounter when reading a bmp file
 pub enum BMPError {
     /// The format of the file doesn't match the specification
@@ -82,5 +90,16 @@ struct Header {
 
 // This assumes we're parsing the header from the start of the slice
 fn parse_file_header(data: &[u8]) -> BMPResult<FileHeader> {
-    unimplemented!()
+    if data.len() < 14 {
+        return invalid_format("insufficient length");
+    }
+    if data[0] != 66 || data[1] != 77 {
+        return invalid_format("header didn't start with 'BM'");
+    }
+    let size = u32_le(&data[2..]);
+    if data[6] != 0 || data[7] != 0 || data[8] != 0 || data[9] != 0 {
+        return invalid_format("reserved bytes not 0");
+    }
+    let offset = u32_le(&data[10..]);
+    Ok(FileHeader { size, offset })
 }
